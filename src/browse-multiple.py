0309223e038bf6774f3be90a183a0c0297e8816e
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-08-12 22:39:37 -0400

    add watchdog to output directory with threading

--- src/browse-multiple.py
+++ src/browse-multiple.py
@@ -1,9 +1,11 @@
+import threading
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
-import os
 import json
 from tinytag import TinyTag, TinyTagException
+
+from AudioSABbuilder.src.directorywatcher import Watcher
 
 
 class AudioFile:
@@ -26,17 +28,19 @@ class BrowseFile(Tk):
         self.minsize(600, 400)
         self.maxsize(1000, 600)
 
+        self.outputdir = "E:\SAB\Output" # default APK output
         self.listAudioFiles = []    # stores uploaded files with metadata
 
         self.labelFrame = ttk.LabelFrame(self, text="Open File")
-        self.labelFrame.grid(column=0, row=1, padx=30, pady=20)
+        self.labelFrame.grid(column=0, row=1, padx=50, pady=20)
 
-        self.button()
+        self.browsebutton()
         self.submitbutton()
+        self.outputbutton()
 
-    def button(self):
-        self.button = ttk.Button(self.labelFrame, text="Browse Files", command=self.filedialog)
-        self.button.grid(column=1, row=1)
+    def browsebutton(self):
+        self.browsebutton = ttk.Button(self.labelFrame, text="Browse Files", command=self.filedialog)
+        self.browsebutton.grid(column=1, row=1)
 
     def filedialog(self):
         fileNames = filedialog.askopenfilenames(initialdir="E:/Downloads/", title="Select Files", filetypes=
@@ -63,17 +67,44 @@ class BrowseFile(Tk):
             except TinyTagException:
                 print("Error reading file at " + file)
 
-        self.label = ttk.Label(self.labelFrame, text="")
-        self.label.configure(text="\n".join(fileNames))
-        self.label.grid(column=1, row=2)
+        self.label1 = ttk.Label(self.labelFrame, text="")
+        self.label1.configure(text="\n".join(fileNames))
+        self.label1.grid(column=1, row=2)
+
+    def outputbutton(self):
+        self.outputbutton = ttk.Button(self.labelFrame, text="Select Output Folder", command=self.outputdiaglog)
+        self.outputbutton.grid(column=3, row=1, padx=10)
+        self.label2 = ttk.Label(self.labelFrame, anchor=E, text="(Default: " + self.outputdir + ")")
+        self.label2.grid(column=3, row=2, padx=10)
+
+    def outputdiaglog(self):
+        outputdir = filedialog.askdirectory(initialdir="E:\SAB\Output", title="Select Folder")
+        if outputdir != "":
+            self.outputdir = outputdir
+            self.label2.config(text=self.outputdir)
 
     def submitbutton(self):
         self.submitbutton = ttk.Button(self.labelFrame, text="Submit", command=self.submit)
-        self.submitbutton.grid(column=1, row=3, pady=20)
+        self.submitbutton.grid(column=1, row=4, pady=20)
+
 
     def submit(self):
-        # trigger back-end process here
-        return self.listAudioFiles
+        print("Build started...")
+        self.label3 = ttk.Label(self.labelFrame, anchor=E, justify=RIGHT, text="Build started...")
+        self.label3.grid(column=1, row=5, rowspan=3, padx=10, pady=20)
+        threading.Thread(target=self.process).start()
+        self.browsebutton.config(state=DISABLED)
+        self.outputbutton.config(state=DISABLED)
+        self.submitbutton.config(state=DISABLED)
+        # TODO: run back-end process here
 
+    def process(self):
+        watcher = Watcher(self.outputdir)
+        watcher.run()
+        print("Build completed!")
+        self.label3.config(text="Build completed!")
+        self.submitbutton.config(state=NORMAL)
+        self.browsebutton.config(state=NORMAL)
+        self.outputbutton.config(state=NORMAL)
 root = BrowseFile()
 root.mainloop()
---
+++ src/directorywatcher.py
@@ -0,0 +1,48 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+class Watcher():
+
+    def __init__(self, dir):
+        self.observer = Observer()
+        self.DIRECTORY_TO_WATCH = dir
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while not(Handler.isUpdated):
+                time.sleep(1)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.stop()
+
+class Handler(PatternMatchingEventHandler):
+    patterns = ["*.apk"]
+    isUpdated = False
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print("File created: %s.", event.src_path)
+            Handler.isUpdated = True
+
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print("File modified: %s.", event.src_path)
+            Handler.isUpdated = True
+
+
+
+if __name__ == '__main__':
+    w = Watcher("E:\SAB\Output")
+    w.run()
